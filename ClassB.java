8958f5ce27232b55d8c4fd443863b11d082d9d26
Priyanaru96 <31105187+saipriyanka96@users.noreply.github.com>
2017-09-24 17:50:07 +0530

    Add files via upload

---
+++ ClassB.java
@@ -0,0 +1,35 @@
+package mypack1;
+//Package is a grouping of related types providing access protection and name 
+//space management
+//create package name as my
+import mypack.*;
+//import is make class and interface available ad we are importing
+//the behavior of mypack
+import mypack.ClassA;//or it giving access to .classfile in this package
+//importing classA methods
+public class ClassB {
+	//public keyword is used in the declaration of a class,method or 
+	//field;public classes,method and fields can be accessed by the 
+	//members of any class.
+	//class defines instance and class fields,methods and inner classes as
+	//well as specifying the interfaces the class implements and the 
+		//immediate superclass of the class
+
+	//created class name as tree
+	public static void main(String[] args) 
+	//static is used for memory management
+	//void is used to define the return type of the method
+	//String[] args is actually an array of java.lang.String type and it's name is args here
+		{
+		// TODO Auto-generated method stub
+		System.out.println("The properties of a dog is:");
+		//system is final class
+		//out is a static member of system class and type printStream
+		//println is method of printStream class.ln means nextline
+		ClassA demo=new ClassA();//creating an object
+		System.out.println("here the classA methods have been called in another package");
+		demo.bark();//here displaying the properties
+		demo.run();//here displaying the properties
+	}
+
+}
